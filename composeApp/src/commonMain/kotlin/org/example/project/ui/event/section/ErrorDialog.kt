81185913ceebc2a7386707949f1e17c197486945
iroha.mitsuyama <iroha.mitsuyama@irohamitsuyamanoMacBook-Pro-2.local>
2025-06-14 23:38:34 +0900

    chore: add some texts to strings.xml

--- composeApp/src/commonMain/kotlin/org/example/project/ui/event/section/ErrorDialog.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/event/section/ErrorDialog.kt
@@ -7,7 +7,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import pickupconnpassevents.composeapp.generated.resources.Res
+import pickupconnpassevents.composeapp.generated.resources.error_dialog_button
+import pickupconnpassevents.composeapp.generated.resources.error_dialog_title
 import theming.AppTheme
 
 @Composable
@@ -18,11 +22,11 @@ fun ErrorDialog(
     AlertDialog(
         modifier = Modifier.size(300.dp, 220.dp),
         onDismissRequest = onDismissRequest,
-        title = { Text(text = "Error") },
+        title = { Text(text = stringResource(Res.string.error_dialog_title)) },
         text = { Text(text = message) },
         confirmButton = {
             Button(onClick = onDismissRequest) {
-                Text(text = "OK")
+                Text(text = stringResource(Res.string.error_dialog_button))
             }
         }
     )
