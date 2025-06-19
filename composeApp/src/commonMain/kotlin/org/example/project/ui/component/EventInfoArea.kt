5a4cc16ca1a82b54cad914eca38d25a73d7f29a8
iroha.mitsuyama <iroha.mitsuyama@irohamitsuyamanoMacBook-Pro-2.local>
2025-06-19 23:41:02 +0900

    chore: remove unnecessary code

--- composeApp/src/commonMain/kotlin/org/example/project/ui/component/EventInfoArea.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/component/EventInfoArea.kt
@@ -2,14 +2,11 @@ package org.example.project.ui.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow.Companion.Ellipsis
-import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.stringResource
 import pickupconnpassevents.composeapp.generated.resources.Res
 import pickupconnpassevents.composeapp.generated.resources.event_item_location
